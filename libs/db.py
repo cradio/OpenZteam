bf788d0fe3ec22737f04e61c6e39fae16df7ae1d
cRadio <38406005+cradio@users.noreply.github.com>
2020-11-21 18:40:51 +0300

    Nothing new, reformatted

--- libs/db.py
+++ libs/db.py
@@ -1,45 +1,49 @@
-#cRadio 2020
+# cRadio 2020
 import threading
 import time
-import io #because of charmap errors
+import io  # because of charmap errors
+
 
 class DB:
-    cntDeleted=0
-    cntLines=0
-    cntPublic=0
-    cntChecked=0
-    cntPrivateMYR=0
-    cntPrivateZabugor=0
-    percentPrivate=0
-    MYRDomains=["ru","kz","ua","by"]
-    DB=[]
+    cntDeleted = 0
+    cntLines = 0
+    cntPublic = 0
+    cntChecked = 0
+    cntPrivateMYR = 0
+    cntPrivateZabugor = 0
+    percentPrivate = 0
+    MYRDomains = ["ru", "kz", "ua", "by"]
+    DB = []
 
-    def __init__(self,dbPath):
-        self.dbPath=dbPath
+    def __init__(self, dbPath):
+        self.dbPath = dbPath
 
     def readDBThread(self):
-        with io.open(self.dbPath,"r", encoding="utf-8") as dbFile:
+        with io.open(self.dbPath, "r", encoding="utf-8") as dbFile:
             for line in dbFile:
-                xline=line.replace(';',':').replace(" ","").replace("\t","").replace("\n","")
-                if line=="" or xline=="":
+                xline = line.replace(';', ':').replace(" ", "").replace("\t", "").replace("\n", "")
+                if line == "" or xline == "":
                     continue
-                self.cntLines+=1
-                if (xline[:1].isalpha() or xline[:1].isnumeric() or xline[:1].startswith("_")) and (":" in xline) and ("@" in xline):
+                self.cntLines += 1
+                if (xline[:1].isalpha() or xline[:1].isnumeric() or xline[:1].startswith("_")) and (":" in xline) and (
+                        "@" in xline):
                     self.DB.append(xline)
                 else:
-                    self.cntDeleted+=1
-                    #print(f"----BAD----\t{xline}")
+                    self.cntDeleted += 1
+                    # print(f"----BAD----\t{xline}")
 
     def readDB(self):
-        DBThread=threading.Thread(target=self.readDBThread)
+        DBThread = threading.Thread(target=self.readDBThread)
         DBThread.start()
-        awaittext="Reading DB"
+        awaittext = "Reading DB"
         while threading.active_count() > 1:
             for dot in range(4):
-                print(f"\r[Bad: {self.cntDeleted} ({round((100*self.cntDeleted/(1 if self.cntLines==0 else self.cntLines)),2)}%)| Total: {self.cntLines}] "+awaittext+"."*dot,end=" "*3)
+                print(
+                    f"\r[Bad: {self.cntDeleted} ({round((100 * self.cntDeleted / (1 if self.cntLines == 0 else self.cntLines)), 2)}%)| Total: {self.cntLines}] " + awaittext + "." * dot,
+                    end=" " * 3)
                 time.sleep(.3)
-        print(f"\n[DB] Done. Loaded {self.cntLines-self.cntDeleted} lines")
+        print(f"\n[DB] Done. Loaded {self.cntLines - self.cntDeleted} lines")
 
-    def writeDB(self,path):
-        with io.open(path,"w", encoding="utf-8") as dbFile:
-            dbFile.write(''.join(line+'\n' for line in self.DB))
+    def writeDB(self, path):
+        with io.open(path, "w", encoding="utf-8") as dbFile:
+            dbFile.write(''.join(line + '\n' for line in self.DB))
--- libs/myrz_api.py
+++ libs/myrz_api.py
@@ -1,40 +1,43 @@
-#cRadio 2020
+# cRadio 2020
 import requests as rq
 import json
 import threading
 
-stdResolveText=" Here are some tips to solve your problem\n\t1) Make sure you have the latest version from github\n\t2) Up to date? Open an issue\nKTHXBYE"
+stdResolveText = "Here are some tips to solve your problem\n\t1) Make sure you have the latest version from " \
+                 "github\n\t2) Up to date? Open an issue\nKTHXBYE "
+
+
 class API:
-    apiBaseUrl="http://myrz.org/api/"
-    headers={'User-Agent': 'Chrome'}
-    validated=False
-    plus=False
-    tmpDB=[]
+    apiBaseUrl = "http://myrz.org/api/"
+    headers = {'User-Agent': 'Chrome'}
+    validated = False
+    plus = False
+    tmpDB = []
 
-    def __init__(self,token):
-        self.token=token
+    def __init__(self, token):
+        self.token = token
 
-    def checkStatusCode(self,code):
+    def checkStatusCode(self, code):
         if code == 200:
             pass
         elif code == 404:
-            print("API directory was not found on this server."+stdResolveText)
+            print("API directory was not found on this server." + stdResolveText)
             exit()
         elif code == 403:
-            print("API server responded with 403 (Access denied)."+stdResolveText)
+            print("API server responded with 403 (Access denied)." + stdResolveText)
             exit()
         else:
-            print("IDK what's happened but we got an error: "+code)
+            print("IDK what's happened but we got an error: " + code)
 
     def validateKey(self):
-        conn = rq.get(self.apiBaseUrl+"check.php?plus=1&key="+self.token,headers=self.headers)
-        self.checkStatusCode(conn.status_code) #only once
-        tmp=json.loads(conn.text)
+        conn = rq.get(self.apiBaseUrl + "check.php?plus=1&key=" + self.token, headers=self.headers)
+        self.checkStatusCode(conn.status_code)  # only once
+        tmp = json.loads(conn.text)
         if "Access" in tmp["message"]:
-            self.validated=True
+            self.validated = True
             print("Key: Valid")
             if "Access" in tmp["plus"]:
-                self.plus=True
+                self.plus = True
                 print("Plus: Valid")
                 return 2
             return 1
@@ -42,26 +45,25 @@ class API:
             print("Key: Invalid")
             return 0
 
-    def searchValidThread(self,db_part):
-        conn = rq.post(self.apiBaseUrl+"part_search.php",data={"key":self.token,"type":"noinsert","lines":db_part},headers=self.headers)
-        tmp=json.loads(conn.text)
-        list=[]
+    def searchValidThread(self, db_part):
+        conn = rq.post(self.apiBaseUrl + "part_search.php",
+                       data={"key": self.token, "type": "noinsert", "lines": db_part}, headers=self.headers)
+        tmp = json.loads(conn.text)
+        list = []
         for entry in tmp:
             if entry["is_private"]:
                 list.append(entry["line"])
         self.tmpDB.extend(list)
 
-
-    def searchValid(self,db):
-        bulk=len(db)
-        threads=[]
-        while bulk>0:
-            db_part=" ".join(db[len(db)-bulk:len(db)-bulk+299])
-            searchThread = threading.Thread(target=self.searchValidThread,args=(db_part,))
+    def searchValid(self, db):
+        bulk = len(db)
+        threads = []
+        while bulk > 0:
+            db_part = " ".join(db[len(db) - bulk:len(db) - bulk + 299])
+            searchThread = threading.Thread(target=self.searchValidThread, args=(db_part,))
             threads.append(searchThread)
-            bulk-=300
+            bulk -= 300
         while threading.active_count() > 1:
-            print(f"\r[Running][Threads:{threading.active_count()-1} Ready:{len(self.tmpDB)}]",end="")
+            print(f"\r[Running][Threads:{threading.active_count() - 1} Ready:{len(self.tmpDB)}]", end="")
             for i in range(len(threads)):
-                
-
+                pass
